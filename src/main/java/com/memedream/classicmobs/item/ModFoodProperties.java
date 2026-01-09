14664d442a5329757fdda13702d05af2109eca71
jodi / jodlodi <88628285+jodlodi@users.noreply.github.com>
2026-01-09 18:06:00 +0100

    Meat On The Bone.

--- src/main/java/com/memedream/classicmobs/item/ModFoodProperties.java
+++ src/main/java/com/memedream/classicmobs/item/ModFoodProperties.java
@@ -3,6 +3,7 @@ package com.memedream.classicmobs.item;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Items;
 
 public class ModFoodProperties {
     // Nutrition is 1 per half hunger refilled
@@ -11,16 +12,16 @@ public class ModFoodProperties {
     public static final FoodProperties COOKED_DODO = new FoodProperties.Builder().nutrition(10).saturationModifier(0.85f).effect(() -> new MobEffectInstance(MobEffects.SATURATION, 200), 1.0f).build();
     public static final FoodProperties ROTTEN_FLESH_BLOCK = new FoodProperties.Builder().nutrition(36).saturationModifier(0.1f).effect(() -> new MobEffectInstance(MobEffects.HUNGER, 600, 9), 0.8f).build();
     public static final FoodProperties SPIDER_EYE_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(0.8f).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 9), 1.0f).build();
-    public static final FoodProperties RAW_BEEF_BLOCK = new FoodProperties.Builder().nutrition(27).saturationModifier(1.62f).build();
-    public static final FoodProperties COOKED_BEEF_BLOCK = new FoodProperties.Builder().nutrition(64).saturationModifier(7.2f).build();
-    public static final FoodProperties RAW_MUTTON_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(2.7F).build();
-    public static final FoodProperties COOKED_MUTTON_BLOCK = new FoodProperties.Builder().nutrition(54).saturationModifier(7.2F).build();
-    public static final FoodProperties RAW_PORK_BLOCK = new FoodProperties.Builder().nutrition(27).saturationModifier(2.7F).build();
-    public static final FoodProperties COOKED_PORK_BLOCK = new FoodProperties.Builder().nutrition(64).saturationModifier(7.2f).build();
-    public static final FoodProperties RAW_RABBIT_BLOCK = new FoodProperties.Builder().nutrition(27).saturationModifier(2.7F).build();
-    public static final FoodProperties COOKED_RABBIT_BLOCK = new FoodProperties.Builder().nutrition(45).saturationModifier(5.4F).build();
-    public static final FoodProperties RAW_CHICKEN_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(2.7F).effect(() -> new MobEffectInstance(MobEffects.HUNGER, 600, 9), 0.3F).build();
-    public static final FoodProperties COOKED_CHICKEN_BLOCK = new FoodProperties.Builder().nutrition(54).saturationModifier(5.4F).build();
+    public static final FoodProperties RAW_BEEF_BLOCK = new FoodProperties.Builder().nutrition(27).saturationModifier(1.62f).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties COOKED_BEEF_BLOCK = new FoodProperties.Builder().nutrition(64).saturationModifier(7.2f).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties RAW_MUTTON_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(2.7F).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties COOKED_MUTTON_BLOCK = new FoodProperties.Builder().nutrition(54).saturationModifier(7.2F).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties RAW_PORK_BLOCK = new FoodProperties.Builder().nutrition(27).saturationModifier(2.7F).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties COOKED_PORK_BLOCK = new FoodProperties.Builder().nutrition(64).saturationModifier(7.2f).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties RAW_RABBIT_BLOCK = new FoodProperties.Builder().nutrition(27).saturationModifier(2.7F).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties COOKED_RABBIT_BLOCK = new FoodProperties.Builder().nutrition(45).saturationModifier(5.4F).usingConvertsTo(Items.BONE).build();
+    public static final FoodProperties RAW_CHICKEN_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(2.7F).usingConvertsTo(Items.BONE).effect(() -> new MobEffectInstance(MobEffects.HUNGER, 600, 9), 0.3F).build();
+    public static final FoodProperties COOKED_CHICKEN_BLOCK = new FoodProperties.Builder().nutrition(54).saturationModifier(5.4F).usingConvertsTo(Items.BONE).build();
     public static final FoodProperties RAW_SALMON_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(0.9F).build();
     public static final FoodProperties COOKED_SALMON_BLOCK = new FoodProperties.Builder().nutrition(54).saturationModifier(7.2F).build();
     public static final FoodProperties RAW_COD_BLOCK = new FoodProperties.Builder().nutrition(18).saturationModifier(0.9F).build();
