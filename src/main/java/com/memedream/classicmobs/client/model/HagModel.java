b538e1b20eea33b5e53dee9039bc6de29f764f52
MemeDreamXD <exalvatorum@gmail.com>
2026-03-20 17:23:01 -0400

    Revert "hag fixes (smh)"
    This reverts commit 8c3ba735527bae3bca8e98e9ba01fbe470e3e44c.

--- src/main/java/com/memedream/classicmobs/client/model/HagModel.java
+++ src/main/java/com/memedream/classicmobs/client/model/HagModel.java
@@ -34,30 +34,30 @@ public class HagModel extends EntityModel<ArmedEntityRenderState> implements Arm
 
         PartDefinition arm_left = partdefinition.addOrReplaceChild("arm_left", CubeListBuilder.create().texOffs(48, 31).addBox(0.0F, -2.0F, -2.0F, 4.0F, 16.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, 8.0F, 0.0F));
 
-        PartDefinition arm_right = partdefinition.addOrReplaceChild("arm_right", CubeListBuilder.create().texOffs(48, 31).mirror().addBox(-4.0F, -2.0F, -2.0F, 4.0F, 16.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-6.0F, 8.0F, 0.0F));
+        PartDefinition arm_right = partdefinition.addOrReplaceChild("arm_right", CubeListBuilder.create().texOffs(48, 51).addBox(-4.0F, -2.0F, -2.0F, 4.0F, 16.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, 8.0F, 0.0F));
 
         PartDefinition leg_left = partdefinition.addOrReplaceChild("leg_left", CubeListBuilder.create().texOffs(0, 52).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(3.0F, 16.0F, 0.0F));
 
-        PartDefinition leg_right = partdefinition.addOrReplaceChild("leg_right", CubeListBuilder.create().texOffs(0, 52).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-3.0F, 16.0F, 0.0F));
+        PartDefinition leg_right = partdefinition.addOrReplaceChild("leg_right", CubeListBuilder.create().texOffs(16, 52).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 16.0F, 0.0F));
 
         PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(48, 0).addBox(-4.0F, -5.0F, -6.0F, 8.0F, 8.0F, 6.0F, new CubeDeformation(0.0F))
-            .texOffs(0, 0).addBox(-1.5F, -4.0F, -7.0F, 3.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
-            .texOffs(48, 14).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 13.0F, 4.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 10.0F, -6.0F));
+                .texOffs(32, 52).addBox(-1.5F, -4.0F, -7.0F, 3.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(48, 14).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 13.0F, 4.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 10.0F, -6.0F));
 
-        PartDefinition tooth5_r1 = head.addOrReplaceChild("tooth5_r1", CubeListBuilder.create().texOffs(0, 11).addBox(-1.0F, -1.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 2.0F, -6.0F, 0.1309F, 0.0F, 0.0F));
+        PartDefinition tooth5_r1 = head.addOrReplaceChild("tooth5_r1", CubeListBuilder.create().texOffs(40, 53).addBox(-1.0F, -1.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 2.0F, -6.0F, 0.1309F, 0.0F, 0.0F));
 
-        PartDefinition tooth4_r1 = head.addOrReplaceChild("tooth4_r1", CubeListBuilder.create().texOffs(0, 11).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 0.0F, -6.0F, -0.3491F, 0.0F, 0.0F));
+        PartDefinition tooth4_r1 = head.addOrReplaceChild("tooth4_r1", CubeListBuilder.create().texOffs(46, 52).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 0.0F, -6.0F, -0.3491F, 0.0F, 0.0F));
 
-        PartDefinition tooth3_r1 = head.addOrReplaceChild("tooth3_r1", CubeListBuilder.create().texOffs(0, 11).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.0F, 0.0F, -6.0F, -0.0873F, 0.0F, 0.0F));
+        PartDefinition tooth3_r1 = head.addOrReplaceChild("tooth3_r1", CubeListBuilder.create().texOffs(44, 52).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.0F, 0.0F, -6.0F, -0.0873F, 0.0F, 0.0F));
 
-        PartDefinition tooth2_r1 = head.addOrReplaceChild("tooth2_r1", CubeListBuilder.create().texOffs(0, 11).addBox(-1.0F, -1.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 2.0F, -6.0F, 0.6545F, 0.0F, 0.0F));
+        PartDefinition tooth2_r1 = head.addOrReplaceChild("tooth2_r1", CubeListBuilder.create().texOffs(42, 52).addBox(-1.0F, -1.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 2.0F, -6.0F, 0.6545F, 0.0F, 0.0F));
 
-        PartDefinition tooth1_r1 = head.addOrReplaceChild("tooth1_r1", CubeListBuilder.create().texOffs(0, 11).addBox(-1.0F, -1.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 2.0F, -6.0F, 0.2182F, 0.0F, 0.0F));
+        PartDefinition tooth1_r1 = head.addOrReplaceChild("tooth1_r1", CubeListBuilder.create().texOffs(40, 52).addBox(-1.0F, -1.0F, 0.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 2.0F, -6.0F, 0.2182F, 0.0F, 0.0F));
 
         PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 28).addBox(-6.0F, -20.0F, -2.0F, 12.0F, 12.0F, 12.0F, new CubeDeformation(0.0F))
-            .texOffs(0, 0).addBox(-6.0F, -20.0F, -2.0F, 12.0F, 16.0F, 12.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 24.0F, -4.0F));
+                .texOffs(0, 0).addBox(-6.0F, -20.0F, -2.0F, 12.0F, 16.0F, 12.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 24.0F, -4.0F));
 
-        return LayerDefinition.create(meshdefinition, 80, 64);
+        return LayerDefinition.create(meshdefinition, 128, 128);
     }
 
     //TODO: Needs item attack animation
